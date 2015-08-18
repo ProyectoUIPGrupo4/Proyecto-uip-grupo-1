4a97b43f525bbdf8cb30ee2d25b5ea7acc433acb
yeferx11 <yeferson_199@hotmail.com>
2015-08-18 14:20:15 -0500

    Proyecto Bosquejo
    Bosquejo

---
+++ Proyecto/ProyectoApp.py
@@ -0,0 +1,9 @@
+from sys import argv
+from os.path import dirname, join
+from kivy.app import App
+from kivy.uix.videoplayer import VideoPlayer
+class ProyectoApp(App):
+	pass
+
+if __name__ == '__main__':
+	ProyectoApp().run()

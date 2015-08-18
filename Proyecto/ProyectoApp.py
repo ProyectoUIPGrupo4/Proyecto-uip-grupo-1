98715640a30c59509a75c4d4ba3e9a3f3662ba5e
yeferx11 <yeferson_199@hotmail.com>
2015-08-18 17:18:40 -0500

    Bosquejo Termnado
    Bosquejo

--- Proyecto/ProyectoApp.py
+++ Proyecto/ProyectoApp.py
@@ -2,8 +2,8 @@ from sys import argv
 from os.path import dirname, join
 from kivy.app import App
 from kivy.uix.videoplayer import VideoPlayer
+from kivy.uix.screenmanager import Screen
 class ProyectoApp(App):
 	pass
-
 if __name__ == '__main__':
 	ProyectoApp().run()
